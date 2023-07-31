771f17e23331230fdde3f63dfee767ca724337b8
Michael Gattozzi <self@mgattozzi.dev>
2023-07-31 20:21:40 +0000

    Move the help string to a separate file
    Rather than having a bunch of oddly formatted strings for the help
    message, we now can include it via `include_str` and have the message
    reside in it's own help.txt file

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,7 @@ use std::error::Error;
 use std::process;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    const HELP: &str = "\
-          Alternative for Cargo\n\n\
-          Usage: freight [COMMAND] [OPTIONS]\n\n\
-          Commands:\n    \
-              build    Build a Freight or Cargo project\n    \
-              test     Test a Freight or Cargo project\n    \
-              help     Print out this message
-        ";
+    const HELP: &str = include_str!("help.txt");
 
     let mut args = env::args().skip(1);
     match args.next().as_ref().map(String::as_str) {
